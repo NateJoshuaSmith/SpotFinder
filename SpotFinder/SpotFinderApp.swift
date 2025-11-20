f317659abc6430c77206f8bae2121d6398d252c8
Nathan <nathanjsmith95@gmail.com>
2025-11-20 00:44:22 -0800

    Initial Commit

---
+++ SpotFinder/SpotFinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpotFinderApp.swift
+//  SpotFinder
+//
+//  Created by Nathan Smith on 11/20/25.
+//
+
+import SwiftUI
+
+@main
+struct SpotFinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
